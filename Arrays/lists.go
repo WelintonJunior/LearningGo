c3a1e1b5e4e5107d4afb65407243b4721f28f49f
WelintonJunior <welintonjr011@gmail.com>
2024-04-26 21:58:56 -0300

    Arraysgit add .!

--- Arrays/lists.go
+++ Arrays/lists.go
@@ -1,18 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+// Dynamic arrays
 func main() {
 
-	var numbers [5]int = [5]int{3, 2, 4, 6, 6}
+	prices := []float64{10.99, 8.99}
+	//adidicionar valor para o prices
+	prices = append(prices, 5)
 
-	for i := 0; i < len(numbers); i++ {
-		fmt.Print("Number: ", numbers[i], "\n")
-	}
+	// criar outro array com o novo valor
+	// updatedPrices := append(prices, 5)
 
-	featuredNumbers := numbers[1:3]
-	fmt.Print(featuredNumbers)
+	prices = prices[:1]
+
+	fmt.Print( /*updatedPrices, */ prices)
 
 }
+
+// func main() {
+
+// 	var numbers [5]int = [5]int{3, 2, 4, 6, 6}
+
+// 	for i := 0; i < len(numbers); i++ {
+// 		fmt.Print("Number: ", numbers[i], "\n")
+// 	}
+
+// 	// featuredNumbers := numbers[1:3]
+// 	fmt.Print(numbers, cap(numbers), len(numbers))
+
+// }
