64432a7b77e28fae0ccb25a20aae121838af1fa9
WelintonJunior <welintonjr011@gmail.com>
2024-04-27 09:40:31 -0300

    For with arrays and slices

--- Arrays/main.go
+++ Arrays/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	userNames := make([]string)
+	userNames := []string{}
 
 	userNames = append(userNames, "Welinton")
 	userNames = append(userNames, "Yasmin")
 
 	fmt.Println(userNames)
+
+	for index, userName := range userNames {
+		fmt.Printf("Index: %v\nNome: %v\n", index, userName)
+	}
 
 }
