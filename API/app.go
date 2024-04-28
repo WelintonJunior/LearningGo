9c3bd3193f1cb66aad4b21ff87bc56a102efb928
WelintonJunior <welintonjr011@gmail.com>
2024-04-28 10:59:52 -0300

    Criando e utilizando o endpoint post com insomnia

--- API/app.go
+++ API/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,13 +12,28 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":3000")
 	// http.ListenAndServe(":3000", nil)
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"name": "Welinton",
-	})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data.", "error": err})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
